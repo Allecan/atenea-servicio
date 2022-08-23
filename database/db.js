1232af5660137e3c2aa27903f2e7d5d0891ab74e
danieltistoj <jose.tisrey@gmail.com>
2022-08-23 13:42:49 -0600

    base como clase

--- database/db.js
+++ database/db.js
@@ -1,7 +1,24 @@
 import {createPool}  from 'mysql2/promise';
 import { config } from '../config/default.js';
 
-
+ export class DataBasePlanetScale{
+    constructor(){
+        this._pool = this.getConnection()
+    }
+    async getConnection(){
+        const pool = await createPool({
+            database:  config.db.database,
+            user:  config.db.user,
+            host: config.db.host,
+            password: config.db.password,
+            ssl:{
+                rejectUnauthorized: false,
+            }
+        })
+        return pool
+    }
+}
+/*
 const conectarDB  = async  ()=> {
     try {
         const pool = await createPool({
@@ -20,7 +37,8 @@ const conectarDB  = async  ()=> {
     }
     
 }
+*/
 
-export {
-    conectarDB,
-}
+const data = new DataBasePlanetScale()
+const newData = await  data.getConnection()
+console.log(newData)
