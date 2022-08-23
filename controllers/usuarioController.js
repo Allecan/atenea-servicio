e518ce9022a35cf76eb4d9d63071168360f4f09d
danieltistoj <jose.tisrey@gmail.com>
2022-08-22 20:58:11 -0600

    creacion del archivo servicio y controlador

---
+++ controllers/usuarioController.js
@@ -0,0 +1,20 @@
+import servicio from '../services/usuarioServicio.js'
+
+const getUsuarios = (req,res)=> {
+}
+
+const getUnUsuario =(req,res)=> { 
+
+}
+
+const crearNuevoUsuario = (req,res)=> { 
+
+}
+
+const modificarUnUnsuarios = (req,res)=> { 
+
+}
+
+const eliminarUnUsuarios= (req,res)=> { 
+    
+}
---
+++ services/usuarioServicio.js
@@ -0,0 +1,28 @@
+import usuario from '../models/Usuario.js'
+
+const getUsuarios = ()=> {
+}
+
+const getUnUsuario = ()=> { 
+
+}
+
+const crearNuevoUsuario = ()=> { 
+
+}
+
+const modificarUnUnsuarios = ()=> { 
+
+}
+
+const eliminarUnUsuarios = ()=> { 
+
+}
+
+module.exports={
+    getUsuarios,
+    getUnUsuario,
+    crearNuevoUsuario,
+    modificarUnUnsuarios,
+    eliminarUnUsuarios,
+}
