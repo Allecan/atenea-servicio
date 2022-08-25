87fccb292429dcf54ac06b7554d149051fb48a04
Yossu502 <jossugames@gmail.com>
2022-08-24 23:35:31 -0600

    Modificando y agregando modelos de usuario y autenticacion

--- models/Usuario.js
+++
@@ -1,8 +0,0 @@
-export class User{
-    constructor(user){
-        this._id = null
-        this._username = user.username
-        this._email = user.email
-        this._password = user.password
-    }
-}
