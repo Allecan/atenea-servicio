c160894378bd08e22560aacd89267babed28dc2d
Yossu502 <jossugames@gmail.com>
2022-09-07 20:27:13 -0600

    CRUD Users

--- api/Usuario/index.js
+++ api/Usuario/index.js
@@ -1,15 +1,14 @@
-import { FireBase } from '../../store/FireBase.js'
+import { FireBaseAdminSDK } from '../../store/FireBaseAdminSDK.js'
 import { ControllerUser } from './controller.js'
 import { UserRouter } from './routes.js'
 import { User } from '../../models/User.js'
-import { helpers } from '../../lib/helpers.js'
 import { response } from '../../response/response.js'
 import { HttpCode } from '../../response/httpcode.js'
 import { config } from '../../config/default.js'
 
 export const userModel = (expressRouter)=>{
-    const userServices = new FireBase(config.fireBase)
-    const userController = new ControllerUser(userServices, User, helpers.encryptPassword)
+    const userServices = new FireBaseAdminSDK(config.firebaseSDK)
+    const userController = new ControllerUser(userServices, User)
     const userRouter = new UserRouter(expressRouter, userController, response, HttpCode)
     return userRouter._router
 } 
