76e90694138ef26c52fd7ebe9d5e508a3383e5d3
Yossu502 <jossugames@gmail.com>
2022-10-04 00:30:33 -0600

    Obtener todos los estudiantes por grado y obtener solo por grado

--- api/Student/controller.js
+++ api/Student/controller.js
@@ -11,10 +11,13 @@ export class ControllerStudent{
         return response
     }
 
-    async updateStudent(student, uid){
-        const newModel = new this._model(student)
-        const newStudent = Object.assign({}, newModel)
-        const response = await this._service.updateData('Students', uid, newStudent)
+    async getAllStudents(){
+        const response = await this._service.getAllStudentsEnable()
+        return response
+    }
+
+    async getStudentsByGrade(uidGradeRef){
+        const response = await this._service.getEnableStudentsByGrade(uidGradeRef)
         return response
     }
 
@@ -22,6 +25,14 @@ export class ControllerStudent{
         const response = await this._service.getOneData('Students', uid)
         return response
     }
+    
+    async updateStudent(student, uid){
+        const newModel = new this._model(student)
+        const newStudent = Object.assign({}, newModel)
+        const response = await this._service.updateData('Students', uid, newStudent)
+        return response
+    }
+
 
     async deleteStudent(student, uid){
         const response = await this._service.deleteData('Students', uid, student)
