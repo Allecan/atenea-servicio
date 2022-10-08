96b56be80163818fdd8a34c9c553ec7c39e06053
danieltistoj <jose.tisrey@gmail.com>
2022-10-07 18:20:43 -0600

    creacion de esquema de boletin y agregar un boletin en swagger

--- api/Boletin/index.js
+++ api/Boletin/index.js
@@ -13,3 +13,65 @@ export const boletinModel = (expressRouter)=>{
     const boeltinRouter = new BoletinRouter(expressRouter, boletinController, response, HttpCode)
     return boeltinRouter._router
 }
+/**
+ * @swagger
+ * components:
+ *   schemas:
+ *     Bulletin:
+ *       type: object
+ *       properties:
+ *         name_student:
+ *           type: string
+ *           description: The name student.
+ *           example: Pedro
+ *         grade:
+ *           type: string
+ *           description: The grade student.
+ *           example: Quinto 
+ *         teacher:
+ *           type: string
+ *           description: The name teacher
+ *           example: Juan
+ *         year: 
+ *           type: number
+ *           description: school year
+ *           example: 2022
+ *         keyCode:
+ *           type: string
+ *           description: student code
+ *           example: erq2r12313dfa
+ *         courses:
+ *           type: array
+ *           description: student course list
+ *           example: [{}]
+ *       required:
+ *         -name_student
+ *         -grade
+ *         -teacher
+ *         -year
+ *         -keyCode
+ *       example: 
+ *         name_student: "José"
+ *         grade: "Quinto"
+ *         teacher: "Maria"
+ *         year: 2022
+ *         keyCode: dfa2321df
+ */
+
+/**
+ * @swagger
+ * /api/v1/boletin/add-boletin:
+ *   post:
+ *     summary: Create a new bulletin.
+ *     tags: [Bulletin]
+ *     requestBody:  
+ *       required: true
+ *       content: 
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             $ref: '#/components/schemas/Bulletin'
+ *     responses:
+ *       200:
+ *         description: create a new bulletin
+*/
