0fdc3c0bf5045854e6284487f2e4245c6ee38167
Jonathan Martinez <jnyalx100@hotmail.com>
2022-10-16 11:10:07 -0600

    Cambio de modelo "grado"
    Se elimino el campo de "lista de estudiantes" del modelo de los grados.

--- models/Grade.js
+++ models/Grade.js
@@ -3,7 +3,6 @@ export class Grade{
         this.grade_name = grade.grade_name
         this.teacherRef = grade.teacherRef
         this.grade_level = grade.grade_level
-        this.studentsList = grade.studentsList || []
     }
 
 }
