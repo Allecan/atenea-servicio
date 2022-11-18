b418097473e3fc510516f5e3b2a215f62e564b22
danieltistoj <josetisrey@gmail.com>
2022-11-18 14:47:17 -0600

    schema actividad

--- api/Actividad/index.js
+++ api/Actividad/index.js
@@ -12,3 +12,49 @@ export const activityModel = (expressRouter)=>{
     const activityRouter = new ActivityRouter(expressRouter, activityController, response, HttpCode)
     return activityRouter._router
 } 
+/**
+ * @swagger
+ * components:
+ *   schemas:
+ *     Activity:
+ *       type: object
+ *       properties:
+ *         activity_name:
+ *           type: string
+ *           description: The name activity.
+ *           example: actividad 1
+ *         activity_value: 
+ *           type: number
+ *           description: The value activity.
+ *           example: 10
+ *         areaRef:
+ *           type: object
+ *           description: The object activity.
+ *           example: {}
+ *         unit:
+ *           type: number
+ *           description: The activity unit
+ *           example: 1
+ *         enable: 
+ *           type: boolean
+ *           description: The enable activity
+ *           example: true
+ *         isTest:
+ *           type: boolean
+ *           description: test activity
+ *           example: false
+ *       required:
+ *         -activity_name
+ *         -activity_value
+ *         -areaRef
+ *         -unit
+ *         -enable
+ *         -isTest
+ *       example: 
+ *         activity_name: actividad 1
+ *         activity_value: 10
+ *         areaRef: {}
+ *         unit: 2
+ *         enable: false
+ *         isTest: false
+ */
