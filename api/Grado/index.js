e2f3363c108a9657a98105855fe7d21fbfd9b179
danieltistoj <josetisrey@gmail.com>
2022-11-20 00:35:12 -0600

    inicio de documentacion grade

--- api/Grado/index.js
+++ api/Grado/index.js
@@ -12,3 +12,36 @@ export const gradeModel = (expressRouter)=>{
     const gradeRouter = new GradeRouter(expressRouter, gradeController, response, HttpCode)
     return gradeRouter._router
 } 
+/**
+ * @swagger
+ * components:
+ *   schemas:
+ *     Grade:
+ *       type: object
+ *       properties:
+ *         grade_name:
+ *           type: string
+ *           description: El nombre del grado.
+ *           example: Grado de Prueba 2
+ *         teacherRef:
+ *           type: string
+ *           description: id del del profesor.
+ *           example: RWFKfGsTsJcIp31Rf4RvibXAZl62
+ *         levelRef: 
+ *          type: string
+ *          description: el id del nivel
+ *          example: EkNCLha4XHgew4Wm2S2H
+ *         enable: 
+ *          type: boolean
+ *          description: indica si el grado esta activo
+ *          example: true
+ *       required:
+ *         -grade_name
+ *         -teacherRef
+ *         -levelRef
+ *         -enable
+ *       example: 
+ *         grade_name: Grado de Prueba 2
+ *         teacherRef: RWFKfGsTsJcIp31Rf4RvibXAZl62
+ *         levelRef: EkNCLha4XHgew4Wm2S2H
+ */
