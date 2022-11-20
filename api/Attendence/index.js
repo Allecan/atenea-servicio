ee7245a2e980bb7cdbabc28ce39ac62fc9ea191b
danieltistoj <josetisrey@gmail.com>
2022-11-20 00:02:58 -0600

    inicio anttendedce documentacion

--- api/Attendence/index.js
+++ api/Attendence/index.js
@@ -10,3 +10,30 @@ export const attendenceModel = (expressRouter) => {
     const attendenceRouter = new AttendenceRouter(expressRouter, attendenceController, response, HttpCode)
     return attendenceRouter._router
 }
+/**
+ * @swagger
+ * components:
+ *   schemas:
+ *     Attendence:
+ *       type: object
+ *       properties:
+ *         date:
+ *           type: string
+ *           description: La fecha de la asistencia.
+ *           example: 15/02/2023
+ *         gradeRef:
+ *           type: string
+ *           description: id del grado relacionado.
+ *           example: 1sabonHrVsMMqRIsHFEy
+ *         students: 
+ *          type: array
+ *          description: Lista id de alumno con booleano que indica asistencia
+ *          example: [{"student": "FaRk3iTfC5K9B1awXwYN", "attendence": true},{"student": "JvqWHsQtUFNfoST1WGeS", "attendence": true},{"student": "yr2xZoKDvyOhiEbJLctJ", "attendence": true}]
+ *       required:
+ *         -area_name
+ *         -gradeRef
+ *       example: 
+ *         date: Area de Prueba
+ *         gradeRef: 5GHHpOWZe4Cy1yBKsjI4
+ *         students: [{"student": "FaRk3iTfC5K9B1awXwYN", "attendence": true},{"student": "JvqWHsQtUFNfoST1WGeS", "attendence": true},{"student": "yr2xZoKDvyOhiEbJLctJ", "attendence": true}]
+ */
